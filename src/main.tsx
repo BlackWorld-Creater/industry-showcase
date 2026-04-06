70f136bdb60fd9cc52b1f22e93c048b630f3f2d7
Aman Sharma <111blackworld.com>
2026-04-06 13:00:23 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ console.log("BOOSTRAPPING START");
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
