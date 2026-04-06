b878b6af77399ff606253fdad51557036c9882af
Aman Sharma <111blackworld.com>
2026-04-06 12:57:23 +0530

    first Commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+console.log("BOOSTRAPPING START");
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  console.log("ROOT ELEMENT FOUND, MOUNTING...");
+  createRoot(rootElement).render(<App />);
+} else {
+  console.log("ROOT ELEMENT NOT FOUND!");
+}
